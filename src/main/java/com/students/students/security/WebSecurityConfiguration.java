1e032b2be88b12195dc56557e4c0162214817279
Ander Rodriguez <ander.rodriguez@bosonit.com>
2021-06-09 12:00:13 +0200

    Spring security basic. No roles created, had coded user, permitAll.

---
+++ src/main/java/com/students/students/security/WebSecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.students.students.security;
+
+import com.students.students.security.filters.JwtRequestFilter;
+import com.students.students.security.services.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    /** Uncoment lines to enable jwt authentication and authorization.**/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /*http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated() // Lo demás requiere autentificación
+                .and().exceptionHandling()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                //.antMatchers("/admin").hasRole("ADMIN")
+                //.antMatchers("/user").hasAnyRole("ADMIN","USER")
+                //.antMatchers("/").permitAll()
+                //.and().formLogin();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);*/
+
+        http.csrf().disable()
+                .authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
