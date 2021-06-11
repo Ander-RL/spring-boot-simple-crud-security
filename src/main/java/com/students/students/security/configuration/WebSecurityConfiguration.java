ab53e1a03dc9c21d857c64472b736c6519749593
Ander Rodriguez <ander.rodriguez@bosonit.com>
2021-06-11 09:04:43 +0200

    Swagger2 funcional con authorization jwt.

--- src/main/java/com/students/students/security/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/students/students/security/configuration/WebSecurityConfiguration.java
@@ -35,8 +35,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/swagger**").permitAll()
-                .antMatchers("/v2/api-docs").permitAll()
                 .antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated()
                 .and().exceptionHandling()
